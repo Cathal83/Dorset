a299d0f4ce128ebd164b5252c0c3a47745ec2253
DorsetCollegeDublin <javiertorrado@galicia.com>
2018-01-17 15:41:40 +0000

    Dorset College Theme created

---
+++ web/themes/dorsetcollege/js/dorsetcollege.js
@@ -0,0 +1,12 @@
+(function ($) {
+
+  "use strict";
+
+  // Submenu FadeIn-Out Function
+  Drupal.behaviors.customBehavior = {
+    attach: function(context, settings) {
+      // JS code from here
+      
+    }
+  };
+})(jQuery);
