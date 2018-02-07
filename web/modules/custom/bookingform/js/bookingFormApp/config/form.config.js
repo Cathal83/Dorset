80edf0a7860be62589189659678b32b6d3bd4f47
DorsetCollegeDublin <javiertorrado@galicia.com>
2018-02-07 17:34:40 +0000

    Application Form first step work - params on URL

--- web/modules/custom/bookingform/js/bookingFormApp/config/form.config.js
+++ web/modules/custom/bookingform/js/bookingFormApp/config/form.config.js
@@ -1,7 +1,57 @@
 bookingformJS.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('bookingform', {
-      url : '/',
-      templateUrl : '/web/modules/custom/bookingform/js/bookingFormApp/templates/_booking-details.html'
-    })
+    
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+        .state('bookingForm', {
+            url : '/',
+            templateUrl : '/web/modules/custom/bookingform/js/bookingFormApp/templates/_booking-details.html'
+        })
+
+        .state('bookingSteps', {
+            url : '/bookingsteps',
+            templateUrl : '/web/modules/custom/bookingform/js/bookingFormApp/templates/_booking-steps.html'
+        })
+        
 })
- 
+
+bookingformJS.value('formSteps', [
+    { uiSref: 'bookingForm', valid: false },
+    { uiSref: 'bookingSteps', valid: false }
+])
+  
+bookingformJS.run([
+'$rootScope',
+'$state',
+'formSteps',
+function($rootScope, $state, formSteps) {
+
+    // Register listener to watch route changes
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+
+        var canGoToStep = false;
+        // only go to next if previous is valid
+        var toStateIndex = _.findIndex(formSteps, function(formStep) {
+            return formStep.uiSref === toState.name;
+
+        });
+
+        console.log('toStateIndex',toStateIndex)
+        if(toStateIndex === 0) {
+            canGoToStep = true;
+        } else {
+            canGoToStep = formSteps[toStateIndex - 1].valid;
+        }
+        console.log('canGoToStep', toState.name, canGoToStep);
+
+        // Stop state changing if the previous state is invalid
+        if(!canGoToStep) {
+            // Abort going to step
+            event.preventDefault();
+        }
+    });
+    
+}
+])
+  
--- web/modules/custom/bookingform/js/bookingFormApp/controllers/form.controller.js
+++ web/modules/custom/bookingform/js/bookingFormApp/controllers/form.controller.js
@@ -1,9 +1,20 @@
-bookingformJS.controller("formCtrl", function($scope, $http, $filter, $state, dataService, formService) {
-
+bookingformJS.controller("formCtrl", function($scope, $http, $filter, $state, $routeParams, dataService, formService, formSteps) {
+  /**
+   * Default user settings
+   */
   $scope.user = {
     deliverymode: null
 
   }
+  /**
+   * 
+   * Show or hide Product selection depending if there's a product id passed on url
+   * or not.
+   * 
+  **/
+  console.log($routeParams);
+  $scope.showProductSel = true;
+
   // Get Data from services
   dataService.getProducts().then(function(response) {
 
@@ -18,25 +29,78 @@ bookingformJS.controller("formCtrl", function($scope, $http, $filter, $state, da
 
   })
 
-  // Returns delivery modes once course is chosen
-  $scope.courseDelivery = function(productid, user) {
+  // Return Product chosen information
+  $scope.productIsolate = function(productnid, user) {
     
     // Reset delivery mode value
     $scope.user.deliverymode = null;
-    var deliveryTypes = formService.courseDelivery(productid, $scope.products);
+    // Hide Product Select
+    $scope.showProductSel = false;
+
+    var productInfo = formService.courseDelivery(productnid, $scope.products);
+
+    $scope.productInfo = productInfo;
+    $scope.productName = productInfo[0].title;
 
     // In case only one option it selects it by default
-    if(deliveryTypes.length == 1) {
+    if(productInfo.length == 1) {
 
-      $scope.deliveryTypes = deliveryTypes;
-      $scope.user.deliverymode = deliveryTypes[0].delivery_mode;
+      $scope.user.deliverymode = productInfo[0].delivery_mode;
 
-    }
-    else {
-
-      $scope.deliveryTypes = deliveryTypes;
-
-    }
+    } else { }
 
   }
+
+  /**
+   * Form Steps Validation
+   * Also view loading based on current template/view
+  **/
+
+  $scope.formData = {};
+  $scope.formStepSubmitted = false;
+  
+  // Form steps
+  // If validation passes move to the next page
+  var nextState = function(currentState) {
+    // Check the current page and returns the next
+    switch (currentState) {
+
+      case 'bookingForm' :
+        return 'bookingSteps'
+        break;
+
+      case 'bookingSteps' :
+        return 'studentDetails'
+        break;
+
+    }
+  }
+
+  // Gets the form current step
+  var updateValidityOfCurrentStep=function(updatedValidity) {
+
+    var currentStateIndex = _.findIndex(formSteps, function(formStep) {
+      return formStep.uiSref === $state.current.name;
+    });
+    formSteps[currentStateIndex].valid = updatedValidity;
+
+  };
+
+  // Check if form is filled before next step
+  $scope.goToNextSection = function(isFormValid) {
+    // set to true to show all error messages (if there are any)
+    $scope.formStepSubmitted = true;
+    if(isFormValid) {
+      // reset this for next form
+      $scope.formStepSubmitted = false;
+
+      // mark the step as valid so we can navigate to it via the links
+      updateValidityOfCurrentStep(true /*valid */);
+
+      $state.go(nextState($state.current.name));
+    } else {
+      // mark the step as valid so we can navigate to it via the links
+      updateValidityOfCurrentStep(false /*not valid */);
+    }
+  };
 })
