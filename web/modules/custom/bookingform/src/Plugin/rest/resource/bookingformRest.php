01b839d1b1545912c7601131c91e40e87e88584a
DorsetCollegeDublin <javiertorrado@galicia.com>
2018-03-09 11:16:55 +0000

    Linking Application form together

--- web/modules/custom/bookingform/src/Plugin/rest/resource/bookingformRest.php
+++ web/modules/custom/bookingform/src/Plugin/rest/resource/bookingformRest.php
@@ -24,15 +24,60 @@
   */
   class bookingformRest extends ResourceBase {
 
+      private $apiKey = "sk_test_zJpfrkdwd8oaD56vZr8eumPO";
+
+      public function __construct() {
+
+          \Stripe\Stripe::setApiKey($this->apiKey);
+
+      }
+
     /**
      * Responds to entity POST request.
      * @return \Drupal\rest\ResourceResponse 
      * @throws \Symfony\Component\HttpKernel\Exception\HttpException
      *
      */
-    public function post() {
+    public function post($formData) {
 
-      return new ResourceResponse("Working!");
+      $formData = json_decode($formData);
+
+      /**
+       * Creates a customer Element on Stripe
+       */
+      $customer = \Stripe\Customer::create(array(
+
+       'email' => $formData->email,
+       'source' => $formData->tk_id
+
+      ));
+
+     /**
+      * Creates Charge to Stripe from JSON Data
+      */
+     try{
+      $charge = \Stripe\Charge::create(array(
+       'customer' => $customer->id,
+       'description' => $formData->product,
+       'receipt_email' => $formData->email,
+       'amount' => $formData->amount,
+       'currency' => 'EUR',
+       'metadata' => array(
+        'firstname' => $formData->firstName,
+        'lastname' => $formData->lastName
+       )
+      ));
+     } catch (\Stripe\Error\Base $e) {
+
+      $error = $e->getMessage();
+
+     } catch (Exception $e) {
+
+      $error = $e->getMessage();
+
+     }
+     
+     return new ResourceResponse($error);
 
     }
   }
