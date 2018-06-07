47dd79c7cad6e579a705c8eab45fdde7b593e06c
Cathal O'Brien <saturn-graphicmint-3@Saturn-Graphic-Mint.local>
2018-06-07 18:38:03 +0100

    JS Updates

--- web/themes/dorsetcollege/js/dorsetcollege.js
+++ web/themes/dorsetcollege/js/dorsetcollege.js
@@ -7,6 +7,34 @@
     attach: function(context, settings) {
       // JS code from here
       
+      
+      // Get the modal
+      // var modal = document.getElementById('myModal');
+      var modal = document.getElementsByClassName('modal')[3];
+
+      // Get the button that opens the modal
+      this.$btn = document.getElementsByClassName("btn")[3];
+
+      // Get the <span> element that closes the modal
+      this.$span = document.getElementsByClassName("close")[3];
+
+      // When the user clicks on the button, open the modal 
+      this.$btn.onclick = function() {
+          modal.style.display = "block";
+      }
+
+      // When the user clicks on <span> (x), close the modal
+      this.$span.onclick = function() {
+         modal.style.display = "none";
+      }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+          if (event.target == modal) {
+              modal.style.display = "none";
+          }
+      }
+    
     }
   };
 })(jQuery);
