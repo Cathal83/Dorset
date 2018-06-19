47154f77d1bea63efc24b6dfa7ce772192a03ce5
iamjavito <javiertorrado@galicia.com>
2018-06-19 20:00:57 +0100

    Added JS for submenu

--- web/themes/dorsetcollege/js/dorsetcollege.js
+++ web/themes/dorsetcollege/js/dorsetcollege.js
@@ -5,8 +5,23 @@
   // Submenu FadeIn-Out Function
   Drupal.behaviors.customBehavior = {
     attach: function(context, settings) {
+      // Menu Navigation
+      $("#block-mainnavigation .menu-lv-0 > .menu-item--expanded").mouseenter(function () {
+        $(this).find(".sub-menu").animate({ height: "toggle" }, 300);
+
+      }).mouseleave(function () {
+        $(this).find(".sub-menu").animate({ height: "toggle" }, 300);
+
+      });
+      $(".sub-menu").change(function (e) {
+        $(".sub-menu").trigger("mouseleave");
+
+      });
+      $(".sub-menu").click(function (e) {
+        $(".sub-menu").trigger("mouseleave");
+
+      });
       // JS code from here
-      
       $('.btn-modal').click(function() {
 
         var ori = $(this).attr("id"); 
