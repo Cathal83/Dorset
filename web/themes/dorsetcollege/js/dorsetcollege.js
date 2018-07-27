2ac55ecd6834447b07f858d70f89658ede17854e
iamjavito <javiertorrado@galicia.com>
2018-07-27 18:14:13 +0100

    Scroll

--- web/themes/dorsetcollege/js/dorsetcollege.js
+++ web/themes/dorsetcollege/js/dorsetcollege.js
@@ -5,6 +5,20 @@
   // Submenu FadeIn-Out Function
   Drupal.behaviors.customBehavior = {
     attach: function(context, settings) {
+      $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+          if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+              $('html, body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
       // Menu Navigation
       $("#block-dorsetcollege-main-menu .menu-lv-0 > .menu-item--expanded").mouseenter(function () {
         //$(this).find(".sub-menu").animate({ height: "toggle" }, 600);
